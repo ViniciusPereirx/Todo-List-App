63bad241bfbae39f9c1ec232c596087753ec017d
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2023-10-05 00:16:33 -0300

    Todo List App

--- js/index.js
+++ js/index.js
@@ -2,10 +2,31 @@ const wrapper = document.querySelector(".wrapper");
 const menuBtn = document.querySelector(".menu-btn");
 const backBtn = document.querySelector(".back-btn");
 
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addTaskForm = document.querySelector(".add-task");
+const blackBackdrop = document.querySelector(".black-backdrop");
+
 const toggleScreen = () => {
   wrapper.classList.toggle("show-category");
+};
+
+const toggleAddTaskForm = () => {
+  addTaskForm.classList.toggle("active");
+  blackBackdrop.classList.toggle("active");
+  addTaskBtn.classList.toggle("active");
 };
 
 //Eventos
 menuBtn.addEventListener("click", toggleScreen);
 backBtn.addEventListener("click", toggleScreen);
+
+addTaskBtn.addEventListener("click", toggleAddTaskForm);
+blackBackdrop.addEventListener("click", toggleAddTaskForm);
+
+// Adicionar tarefas e categorias
+
+let categories = [];
+
+let tasks = [];
+
+// --------------------------------------------------------------
